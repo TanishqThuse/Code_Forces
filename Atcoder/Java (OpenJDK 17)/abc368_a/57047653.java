a3d43a37f6ac24607c3791b2711c06ab00ef03fb
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-12-29 20:16:23 +0530

    Committing files

---
+++ Atcoder/Java (OpenJDK 17)/abc368_a/57047653.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.addAll(list.subList(n - k, n));
+        result.addAll(list.subList(0, n - k));
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+            if (i != result.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
